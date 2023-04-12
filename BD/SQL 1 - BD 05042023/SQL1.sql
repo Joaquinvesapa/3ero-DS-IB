b129c654a3d6651e9d027f39dc941ad787ddba52
joaquinvesapa <joaquinvesapa@mail.com>
2023-04-12 10:27:43 -0300

    inserts in SQL1

--- BD/SQL 1 - BD 05042023/SQL1.sql
+++ BD/SQL 1 - BD 05042023/SQL1.sql
@@ -32,10 +32,25 @@ CREATE TABLE matriculas(
 
 INSERT INTO carreras(id, nombre) VALUES(1, 'RRHH'),(2, 'DS')
 
-INSERT INTO ALUMNOS VALUES
+
+INSERT INTO ALUMNOS (id, nombre, fecha_nac, sexo, carrera_id VALUES
 (1, "JUAN", "1990/01/01", "M", 1),
 (2, "LUCAS", "1998/01/03", "M", 1), 
 (3, "MARIA", "1999/12/03", "F", 1),
 (4, "MALENA", "1997/12/22", "F", 2),
 (5, "JUAN", "1999/11/01", "M", 2),
 (6, "MARIANA", "2000/11/03", "F", 2);
+
+INSERT INTO cursos(id, nombre, cant_cred, carrera_id) VALUES (1, "INGLES", 100, 1), (2, "ING DE SOFT", 150, 2), (3, "PROGRAMACION", 120, 2);
+
+INSERT INTO matriculas(id, alumno_id, curso_id, fecha) VALUES
+(1,1,1,"2021-03-10")
+(2,1,2,"2021-03-10")
+(3,1,3,"2021-03-10")
+(4,3,1,"2021-03-11")
+(5,4,2,"2021-03-10")
+(6,4,3,"2021-03-11")
+(7,5,1,"2021-03-12")
+(8,5,3,"2021-03-12")
+(9,6,1,"2021-03-10")
+(10,6,1,"2021-03-10")
