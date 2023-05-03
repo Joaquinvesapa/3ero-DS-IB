a21d2ad91fb3d326a0c323b03a3dcf083a88da2e
joaquinvesapa <joaquinvesapa@gmail.com>
2023-05-03 12:26:16 -0300

    mostrar multiplos por numero

--- Programacion 2/Clase 03-05-2023/app.js
+++ Programacion 2/Clase 03-05-2023/app.js
@@ -1,1 +1,17 @@
-console.log('Hola Mundo')
+elResultado = document.querySelector('.text');
+
+let num = prompt('Ingrese un numero')
+let vacio = ''
+
+if( num.length !== 0 && !isNaN(parseInt(num))){
+    const text = `El numero ingresado es: ${num}`;
+    document.querySelector('.text').innerHTML = text;
+    for(let i = 0; i < 11; i++){
+        let row = `<tr> ${num} X ${i} = ${i*num}<tr>`
+        document.getElementById('table').innerHTML += row
+    }
+}else{
+    alert('invalido')
+}
+
+// console.log(num)
