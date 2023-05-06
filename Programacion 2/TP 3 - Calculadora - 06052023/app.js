f247e31e169bb49c65cc50cd621e7b28b14701d5
Joaquin Vesco Aparicio <joaquinvesapa@gmail.com>
2023-05-06 17:10:10 -0300

    write calculator

--- Programacion 2/TP 3 - Calculadora - 06052023/app.js
+++ Programacion 2/TP 3 - Calculadora - 06052023/app.js
@@ -1,6 +1,24 @@
-const input1 = document.getElementById("input1");
+//number inputs
+// const in1 = document.getElementById("in1");
+// const in2 = document.getElementById("in2");
+// const in3 = document.getElementById("in3");
+// const in4 = document.getElementById("in4");
+// const in5 = document.getElementById("in5");
+// const in6 = document.getElementById("in6");
+// const in7 = document.getElementById("in7");
+// const in8 = document.getElementById("in8");
+// const in9 = document.getElementById("in9");
+// const in0 = document.getElementById("in0");
+const inputs = document.querySelectorAll("input");
 const curCalculate = document.getElementById("curCalculate");
-
-input1.addEventListener("click", function () {
-  curCalculate.value += input1.value;
+// console.log(inputs);
+inputs.forEach(function (el) {
+  el.addEventListener("click", function () {
+    if (el.value === "AC") {
+      curCalculate.value = "";
+    } else {
+      curCalculate.value += el.value;
+    }
+    // curCalculate.value += el.value;
+  });
 });
