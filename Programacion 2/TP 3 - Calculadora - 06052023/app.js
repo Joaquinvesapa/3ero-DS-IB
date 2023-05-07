410f48cd21f989011898ff628a215352368f19a7
Joaquin Vesco Aparicio <joaquinvesapa@gmail.com>
2023-05-07 11:28:36 -0300

    function eval for calculator

--- Programacion 2/TP 3 - Calculadora - 06052023/app.js
+++ Programacion 2/TP 3 - Calculadora - 06052023/app.js
@@ -1,4 +1,4 @@
-//number inputs
+//Inputs of numbers
 // const in1 = document.getElementById("in1");
 // const in2 = document.getElementById("in2");
 // const in3 = document.getElementById("in3");
@@ -9,16 +9,18 @@
 // const in8 = document.getElementById("in8");
 // const in9 = document.getElementById("in9");
 // const in0 = document.getElementById("in0");
+
 const inputs = document.querySelectorAll("input");
 const curCalculate = document.getElementById("curCalculate");
-// console.log(inputs);
+
 inputs.forEach(function (el) {
   el.addEventListener("click", function () {
     if (el.value === "AC") {
       curCalculate.value = "";
+    } else if (el.value === "=") {
+      curCalculate.value = eval(curCalculate.value);
     } else {
       curCalculate.value += el.value;
     }
-    // curCalculate.value += el.value;
   });
 });
