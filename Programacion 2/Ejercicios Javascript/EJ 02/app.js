b1b9dfc341d39736ebe54ee1faf0baf390e1a812
Joaquin Vesco Aparicio <joaquinvesapa@gmail.com>
2023-05-19 02:43:26 -0300

    funcionalidad de calculo de formulas

--- Programacion 2/Ejercicios Javascript/EJ 02/app.js
+++ Programacion 2/Ejercicios Javascript/EJ 02/app.js
@@ -11,10 +11,29 @@
 */
 
 const FORMULAS = {
-  "celsius-far": () => (input * 9) / 5 + 32,
-  "celsius-kelvin": () => input + 273.15,
-  "far-celsius": () => ((input - 32) * 5) / 9,
-  "far-kelvin": () => ((input - 32) * 5) / 9 + 273.15,
-  "kelvin-far": () => ((input - 273.15) * 9) / 5 + 32,
-  "kelvin-celsius": () => input - 273.15,
+  "celsius-farenheit": (input) => (input * 9) / 5 + 32,
+  "celsius-kelvin": (input) => input + 273.15,
+  "celsius-roemer": (input) => input * 0.525 + 7.5,
+  "farenheit-celsius": (input) => ((input - 32) * 5) / 9,
+  "farenheit-kelvin": (input) => ((input - 32) * 5) / 9 + 273.15,
+  "farenheit-roemer": (input) => (input - 32) * 0.29167 + 7.5,
+  "kelvin-farenheit": (input) => ((input - 273.15) * 9) / 5 + 32,
+  "kelvin-celsius": (input) => input - 273.15,
+  "kelvin-roemer": (input) => (input - 273.15) * 0.525 + 7.5,
+  "roemer-celsius": (input) => (input - 7.5) / 0.525,
+  "roemer-farengeit": (input) => (input - 7.5) * 3.4286 + 32.0,
+  "roemer-kelvin": (input) => (input - 7.5) / 0.525 + 273.15,
 };
+const temps1 = document.getElementById("temps1");
+const temps2 = document.getElementById("temps2");
+const btnCalc = document.getElementById("btnCalc");
+
+btnCalc.addEventListener("click", () => {
+  if (temps1.value === temps2.value) {
+    alert("Las temperaturas son iguales");
+  } else {
+    const num = document.getElementById("num").value;
+    let formulaName = temps1.value + "-" + temps2.value;
+    console.log(FORMULAS[formulaName](new Number(num)).toFixed(2));
+  }
+});
