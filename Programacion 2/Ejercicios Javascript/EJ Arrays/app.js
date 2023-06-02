60df7a8e15dfe363d723f9d59aadb74d093b1888
Joaquin Vesco Aparicio <joaquinvesapa@gmail.com>
2023-06-02 19:14:30 -0300

    ejercicio 2 de arrays funcionando

---
+++ Programacion 2/Ejercicios Javascript/EJ Arrays/app.js
@@ -0,0 +1,49 @@
+const notasAlumnos = document.querySelector(".notas-alumnos");
+const listAbanderados = document.querySelector(".abanderados");
+const btnCargarNotasAlumnos = document.querySelector(".cargar-notas");
+const btnVerAbanderados = document.querySelector(".ver-abanderados");
+
+// let arreglo = [];
+// for (let i = 0; i < 30; i++) {
+//   let num = Math.random() * (10 - 0) + 1;
+//   arreglo.push(Math.round(num));
+// }
+let arreglo = [];
+
+btnCargarNotasAlumnos.addEventListener("click", () => {
+  notasAlumnos.innerHTML = "";
+  arreglo = [];
+  for (let i = 0; i < 30; i++) {
+    let num = Math.random() * (10 - 1) + 1;
+    arreglo.push([Math.round(num), i]);
+  }
+  arreglo.forEach((nota) => {
+    const li = document.createElement("li");
+    li.textContent = `Nota Alumno N° ${nota[1] + 1}: ${nota[0]}`;
+    li.className = "nota-alumno";
+    notasAlumnos.appendChild(li);
+  });
+});
+
+function compare(a, b) {
+  return b[0] - a[0];
+}
+
+btnVerAbanderados.addEventListener("click", (e) => {
+  e.preventDefault();
+  abanderado(arreglo);
+});
+
+function abanderado(arreglo) {
+  listAbanderados.innerHTML = "";
+  arreglo
+    .sort(compare)
+    .slice(0, 3)
+    .forEach((nota) => {
+      let p = document.createElement("p");
+      p.className = "abanderado";
+      p.textContent = `Nota: ${nota[0]}, Posicion : ${nota[1]}`;
+      listAbanderados.appendChild(p);
+      console.log(`Nota: ${nota[0]}, Posicion : ${nota[1]}`);
+    });
+}
