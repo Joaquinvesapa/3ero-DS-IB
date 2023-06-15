40eab771e8a5ee07bc8cb30bbf6dd8b185acb0a3
joaquinvesapa <joaquinvesapa@gmail.com>
2023-06-15 11:49:26 -0300

    ejercicio de practica parcial 2

--- Programacion 2/Ejercicios Javascript/EJ 01/app.js
+++ Programacion 2/Ejercicios Javascript/EJ 01/app.js
@@ -1,27 +1,30 @@
 const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+
+function esBisiesto(año) {
+  if ((año % 4 === 0 && año % 100 != 0) || año % 400 === 0) {
+    console.log("es");
+    return true;
+  } else {
+    return false;
+  }
+}
 
 btn1.addEventListener("click", function () {
   let año1 = document.getElementById("año1").value;
-
-  function esBisiesto(año) {
-    if (año % 400 === 0) {
-      if (año % 100 === 0) {
-        if (año % 4 === 0) {
-          return true;
-        } else {
-          return false;
-        }
-      } else {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  }
 
   if (esBisiesto(año1)) {
     document.getElementById("ans1").innerHTML = "✅";
   } else {
     document.getElementById("ans1").innerHTML = "❌";
+  }
+});
+
+btn2.addEventListener("click", function () {
+  let año2 = new Date(document.getElementById("año2").value).getFullYear();
+  if (esBisiesto(año2)) {
+    document.getElementById("ans2").innerHTML = "✅";
+  } else {
+    document.getElementById("ans2").innerHTML = "❌";
   }
 });
