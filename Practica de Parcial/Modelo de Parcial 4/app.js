6e4a2fed4853ce9305104309cb08730b85e2f571
joaquinvesapa <joaquinvesapa@gmail.com>
2023-07-04 16:08:19 -0300

    modelo de parcial 4 resuelto

---
+++ Practica de Parcial/Modelo de Parcial 4/app.js
@@ -0,0 +1,37 @@
+const inpText = document.querySelector('.inp-text')
+const inpSub = document.querySelector('.inp-sub')
+const contadorPalabras = document.querySelector('.contador-palabras')
+const divInforme = document.querySelector('.informe')
+
+const PALABRAS = []
+let totalLetras = 0
+let palabraMasLarga = 0
+
+
+
+const emitirInforme = () =>{
+  PALABRAS.forEach(palabra => {
+    totalLetras = totalLetras + palabra.length
+  })
+  
+  const informeHTML = `<p>Promedio de Letras x Pala Cargadas: ${totalLetras/5}</p>
+  
+  <p>Palabra mas larga: Palabra n° ${palabraMasLarga+1} = ${PALABRAS[palabraMasLarga]} </p>
+  <p>Ultima palbra inversa: ${PALABRAS.at(-1).split('').reverse().join('')} </p>`
+  divInforme.innerHTML = informeHTML;
+}
+
+inpSub.addEventListener('click', (e) => {
+  if(inpText.value === ''){
+    alert("Por favor ingrese una palabra")
+  }else{
+    PALABRAS.push(inpText.value)
+    if(PALABRAS.length > 1 && inpText.value.length > PALABRAS[palabraMasLarga].length){
+      palabraMasLarga = PALABRAS.length - 1
+    }
+    console.log(PALABRAS)
+    inpText.value = ''
+    contadorPalabras.textContent = `Palabras cargadas: ${PALABRAS.length}`
+    if(PALABRAS.length === 5)emitirInforme();
+  }
+})
